d3cb5a4e03be514c19b3f6b8a7b8b76daeb029b3
LbRCoder <73874983+LbRCoder@users.noreply.github.com>
2024-02-17 15:33:55 +0300

    Update and rename solution.py to Solution.py

---
+++ restore-ip-addresses/Solution.py
@@ -0,0 +1,22 @@
+#17.02.2024 - LbRCodeR
+class Solution(object):
+    def restoreIpAddresses(self, s):
+
+        def backtrack(start, path):
+            if start == len(s) and len(path) == 4:
+                result.append(".".join(path))
+                return
+            if len(path) > 4:
+                return
+            for i in range(1, 4):
+                if start + i > len(s):
+                    break
+                segment = s[start:start + i]
+                if (len(segment) > 1 and segment[0] == '0') or (i == 3 and int(segment) > 255):
+                    continue
+                backtrack(start + i, path + [segment])
+
+        result = []
+        backtrack(0, [])
+        return result
+
