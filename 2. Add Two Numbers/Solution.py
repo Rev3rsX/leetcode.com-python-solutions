30becb689eeacc033caac03dbf7aa23f9d4b8585
Rev3rsX <73874983+Rev3rsX@users.noreply.github.com>
2024-02-17 16:58:35 +0300

    Create Solution.py

---
+++ 2. Add Two Numbers/Solution.py
@@ -0,0 +1,23 @@
+#Rev3rsX
+class Solution(object):
+
+    def addTwoNumbers(self, l1, l2):
+        dummy_head = ListNode()
+        current = dummy_head
+        carry = 0
+
+        while l1 or l2 or carry:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+
+            total = x + y + carry
+            carry = total // 10
+
+            current.next = ListNode(total % 10)
+            current = current.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        return dummy_head.next
